4f564621b8f34788c4e9809af524c857d78d5ad7
Matthew Smith <58535096+matthewmsmith@users.noreply.github.com>
2020-05-07 00:27:17 -0400

    Graphs to display

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const alertBanner = document.getElementById('alert');
 const trafficCanvas = document.getElementById('traffic-chart');
 const dailyCanvas = document.getElementById('daily-chart');
+const mobileCanvas = document.getElementById('mobile-chart');
+
+// FORM FIELDS
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
 
 // ALERT BANNER 
 alertBanner.innerHTML = 
@@ -54,6 +60,70 @@ const dailyData = {
   labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'], 
   datasets: [{
     label: '# of Hits',
-    data: ['75', '15', '175', '125', '225', '200', '100']
+    data: ['75', '15', '175', '125', '225', '200', '100'],
+  backgroundColor: '#7477bf',
+  borderWidth: 1
   }]
+};
+const dailyOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  },
+  legend: {
+    display: false
+  }
 }
+let dailyChart = new Chart(dailyCanvas, {
+  type: 'bar',
+  data: dailyData,
+  options: dailyOptions
+})
+
+// MOBILE TRAFFIC OBJECTS
+
+const mobileData = {
+  labels: ['Desktop', 'Tablet', 'Desktop'],
+  datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+       '#7477BF',
+       '78CF82',
+       '51B6C8'
+    ]
+
+  }]
+};
+
+const mobileOptions = {
+  legend: {
+    position: 'right',
+    labels: {
+      boxWidth: 20,
+      fontStyle: 'bold'
+    }
+  }
+}
+let mobileChart = new Chart(mobileCanvas, {
+  type: 'doughnut',
+  data: mobileData,
+  options: mobileOptions
+});
+
+send.addEventListener('click', () => {
+
+  if(user.value === '' && message.value === '') {
+    alert('Please send out user and message field before sending');
+  } else if (user.value === '') {
+    alert('Please fill out user field before sending');
+  } else if (message.value === '') {
+    alert('Please fill out message field before sending');
+  } else {
+    alert(`Message was successfully sent to ${user.value}`);
+  }
+})
